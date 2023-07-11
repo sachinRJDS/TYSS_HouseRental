680365cf9188576417b5b83a50d8bbe65c6b77dd
DELL <DELL@DESKTOP-EFLV044>
2023-07-11 17:51:05 +0530

    1st commit

--- Vtiger/src/main/java/com/houserental/genericutility/ITransform.java
+++ Vtiger/src/main/java/com/houserental/genericutility/ITransform.java
@@ -7,7 +7,7 @@ import org.testng.annotations.ITestAnnotation;
 import org.testng.internal.annotations.IAnnotationTransformer;
 
 public class ITransform implements IAnnotationTransformer{
-
+	//welcome
 	@Override
 	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
 		annotation.setRetryAnalyzer(com.houserental.genericutility.RetryImplementation.class);
